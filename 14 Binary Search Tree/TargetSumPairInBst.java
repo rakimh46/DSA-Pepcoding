f76a8b070c468bc3e1941d862d6cc8323ef3ba84
rakimh46 <rakimh46@gmail.com>
2022-04-14 20:15:14 +0530

    BST

---
+++ 14 Binary Search Tree/TargetSumPairInBst.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.*;
+
+public class TargetSumPairInBst {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
+    public static boolean find(Node node, int ele) {
+        // write your code here
+        if (node == null) {
+            return false;
+        }
+        if (node.data == ele) {
+            return true;
+        } else if (ele > node.data) {
+            return find(node.right, ele);
+        } else {
+            return find(node.left, ele);
+        }
+    }
+
+    public static void taretsum(Node node, int target, Node root) {
+        if (node == null)
+            return;
+        taretsum(node.left, target, root);
+        int comp = target - node.data;
+        if (comp > node.data && find(root, comp)) {
+            System.out.println(node.data + " " + comp);
+        }
+        taretsum(node.right, target, root);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
+            } else {
+                arr[i] = null;
+            }
+        }
+
+        int data = Integer.parseInt(br.readLine());
+
+        Node root = construct(arr);
+        // write your code here
+        taretsum(root, data, root);
+    }
+
+}
