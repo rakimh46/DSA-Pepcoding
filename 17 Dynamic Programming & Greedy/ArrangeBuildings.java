ed932d7008bb59792029aeb61a838390a5d333dd
rakimh46 <rakimh46@gmail.com>
2022-04-22 01:15:43 +0530

    dp

--- 17 Dynamic Programming & Greedy/ArrangeBuildings.java
+++ 17 Dynamic Programming & Greedy/ArrangeBuildings.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class ArrangeBuildings {
-    
+    public static void main(String[] args) {
+        try (Scanner scn = new Scanner(System.in)) {
+            int n = scn.nextInt();
+            long count0=1;
+            long count1=1;
+
+            for (int i = 2; i <=n; i++) {
+                long temp0=count0;
+                count0=count1;
+                count1=temp0+count1;
+            }
+            long ans=(count0+count1)*(count0+count1);
+            System.out.println(ans);
+        }
+    }
 }
